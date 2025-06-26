1ede4cb7bfe5487ba55667ec0fff5c00f64f66ef
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-26 16:44:25 +0200

    Added code to fetch data from an API and print the results

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ console.log("Hello Boolean");
 console.log(chalk.bgBlue("Hello world!"));
 
 console.log(process.env.secret);
+
+const apiUrl = "https://lanciweb.github.io/demo/api/pictures/";
+
+axios.get(apiUrl).then((res) => {
+  console.log(res.data);
+});
